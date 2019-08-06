423a23156ee308b41aa8433d9b2407f6b972dc2b
mingxv <yu.jilong@immomo.com>
2019-08-06 16:10:38 +0800

    展开二叉树

---
+++ src/com/fucking/leetcode/test/TreeNodeSolution.java
@@ -0,0 +1,40 @@
+package com.fucking.leetcode.test;
+
+/**
+ * 给定二叉树，原地展开为链表
+ * 
+ * @author mingxv
+ *
+ * 1 2 5 3 4 null 6
+ */
+public class TreeNodeSolution {
+	public void flatten(TreeNode root) {
+		if(root == null) {
+			return;
+		}
+		flatten(root.left);
+		flatten(root.right);
+		TreeNode lefTreeNode = root.left;
+		TreeNode rightTreeNode = root.right;
+		if(lefTreeNode == null) {
+			return;
+		}
+		TreeNode tempNode = lefTreeNode;
+		while(tempNode.right != null) {
+			tempNode = tempNode.right;
+		}
+		tempNode.right = root.right;
+		root.right = lefTreeNode;
+		root.left = null;
+	}
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+}
