2ac959d416a8d0086f0da17d6206ad613a032e21
mingxv <9698542@qq.com>
2019-08-14 23:14:03 +0800

    delete note

--- src/com/fucking/leetcode/LeetCodeMain.java
+++ src/com/fucking/leetcode/LeetCodeMain.java
@@ -1,9 +1,5 @@
 package com.fucking.leetcode;
 
-import java.util.List;
-
-import com.fucking.leetcode.code.MinStack;
-import com.fucking.leetcode.code.No229;
 import com.fucking.leetcode.code.No231;
 
 public class LeetCodeMain {
