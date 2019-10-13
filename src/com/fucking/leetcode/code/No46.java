87cdaba985dd54b419a010eea04e37a83f8350e6
mingxv <9698542@qq.com>
2019-10-13 19:59:06 +0800

    `

--- src/com/fucking/leetcode/code/No46.java
+++ src/com/fucking/leetcode/code/No46.java
@@ -3,9 +3,9 @@ package com.fucking.leetcode.code;
 import java.util.List;
 
 public class No46 {
-	public List<List<Integer>> permute(int[] nums) {
-
-	}
+//	public List<List<Integer>> permute(int[] nums) {
+//
+//	}
 	
 	
 }
